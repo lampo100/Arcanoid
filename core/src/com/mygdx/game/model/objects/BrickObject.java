8152f9a64192ac5399a99ba52ba7757c9d53add5
Kacper Kamiński <lampo100@gmail.com>
2017-05-20 02:26:37 +0200

    Change brick color

--- core/src/com/mygdx/game/model/objects/BrickObject.java
+++ core/src/com/mygdx/game/model/objects/BrickObject.java
@@ -7,6 +7,10 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
  * Created by Kacper on 2017-05-16.
  */
 public class BrickObject extends GameObject{
+    public BrickObject(){
+        super();
+        this.setColor(88/255f, 14/255, 2/255, 1);
+    }
     @Override
     public void draw(Batch batch, float parentAlpha) {
         batch.end();
